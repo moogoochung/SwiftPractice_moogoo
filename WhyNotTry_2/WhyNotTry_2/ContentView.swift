b52957307b8543345a166901bd10de0559ac3a71
moogoo <moogoo1208@gmail.com>
2025-07-23 15:24:07 +0900

    자동 커밋: 2025-07-23 15:24:07

--- WhyNotTry_2/WhyNotTry_2/ContentView.swift
+++ WhyNotTry_2/WhyNotTry_2/ContentView.swift
@@ -8,14 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    var activities = ["Archery", "Baseball", "Basketball", "Cycling", "Diving", "Football", "Golf", "Hiking", "Ice skating", "Jogging", "Kayaking", "Martial arts", "Mountain biking", "Swimming", "Tennis", "Volleyball"]
+    var colors: [Color] = [.blue, .cyan, .green, .orange, .pink, .purple, .red, .yellow]
+    @State private var selected = "Archery"
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            Text("Why not try..")
+                .font(.title)
+            VStack {
+                Circle()
+                    .fill(colors.randomElement() ?? .blue)
+                    .padding()
+                    .overlay(
+                        Image(systemName: "figure.\(selected.lowercased())")
+                            .font(.system(size: 144))
+                            .foregroundColor(.white)
+                    )
+                Text("\(selected)")
+                    .font(.title)
+            }
+            
+            Spacer()
+            
+            Button("try again") {
+                withAnimation {
+                    selected = activities.randomElement() ?? "Baseball"
+                }
+            }
+            .buttonStyle(.borderedProminent)
         }
-        .padding()
     }
 }
 
